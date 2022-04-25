10b449c8284594a42587916fe052bc4d179c454d
pushpdeepsingh <pushpdeepsinghv@gmail.com>
2022-04-25 15:06:33 +0530

    Strings basics

---
+++ JS/Strings.js
@@ -0,0 +1,32 @@
+let str="I am string";
+console.log(str);
+
+//length of string
+
+let len = str.length;
+console.log(len);
+
+//sliced method
+let sliecedStr = str.slice(0,4);
+console.log(sliecedStr);
+console.log(typeof(sliecedArr));
+
+//replace method
+let replacedstr= str.replace("am","ammie")
+console.log(replacedstr);
+
+//lowercase
+let s="KDMFkmdfpA"
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+
+//concatenate
+let firstname="john";
+let secondname="last";
+console.log(firstname+secondname);
+
+let x="ksmd sdk sd skdm sks d ";
+let arr = x.split(" ");
+console.log(arr);
+
+
