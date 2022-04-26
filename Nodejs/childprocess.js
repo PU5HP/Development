971b379069894bd0e24e04ae893f517907923e59
pushpdeepsingh <pushpdeepsinghv@gmail.com>
2022-04-26 23:14:33 +0530

    Node js basics

---
+++ Nodejs/childprocess.js
@@ -0,0 +1,10 @@
+//child,os,path,etc process is preinstalled in node
+//npm node package manager is a type of google play store
+let cp =require('child_process');
+//console.log(cp)    //cp stores an object
+
+//open calaculator with the help of this file replace with calc in windows
+
+//cp.execFileSync('gnome-calculator')
+//cp.execFileSync('firefox')
+cp.execFileSync('test.js')
