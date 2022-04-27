a06b367ccc8cfea4b6664de916fb0c1ed8f0b03e
pushpdeepsingh <pushpdeepsinghv@gmail.com>
2022-04-27 15:21:13 +0530

    Modularity

---
+++ Nodejs/Modularity/test.js
@@ -0,0 +1,6 @@
+let calc =require("./calculator.js");
+console.log(calc);
+console.log(calc.addition(3,4));
+console.log(calc.subtraction(3,4));
+console.log(calc.multiplication(3,4));
+console.log(calc.division(3,4));
