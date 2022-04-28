ebcb3b5184ac0659070e94973887757932c0f4c7
pushpdeepsingh <pushpdeepsinghv@gmail.com>
2022-04-28 15:20:46 +0530

    Directory crud operations in nodejs

--- Nodejs/fs.js
+++ Nodejs/fs.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 let fs=require('fs');
 let path=require('path');
 //console.log(fs);
@@ -24,8 +25,30 @@ console.log(fs.readFileSync(filepath,'utf-8'));
 //deleting a file
 //fs.unlinkSync(filepath);
 
+//Directory operations
+
+//create 'mydirectory' in Nodejs directory
+
+if(!fs.existsSync("mydirectory"))
+{fs.mkdirSync("mydirectory");}
+else{
+    console.log("Folder already exists")
+}
+//reading an folder
+let pathofdir=__dirname;
+console.log(pathofdir);
+let contentofdir = fs.readdirSync(pathofdir);
+console.log(contentofdir);
+//deleting an empty folder 'mydirectory'
+//fs.rmdirSync('mydirectory');
 
 
+//copying a file testfile.txt from node js folder to modularity folder
 
-
+let srcpath =path.join(__dirname,'testfile.txt');
+console.log(srcpath);
+let despath = path.join(__dirname,'Modularity','testfile.txt');
+console.log(despath);
+//copying the file
+fs.copyFileSync(srcpath,despath);
 
